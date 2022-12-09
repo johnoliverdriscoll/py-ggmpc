e0b1a8a4833ebc1e0178df22a5294a4bb03aad91
John Driscoll <john@bitgo.com>
2022-12-09 10:22:42 -0600

    Faster implementation of get_random_coprime_to

--- src/ggmpc/rand.py
+++ src/ggmpc/rand.py
@@ -1,11 +1,12 @@
-from Crypto.Util import number
+from Crypto.Random.random import getrandbits
+from Crypto.Util.number import getRandomRange
 from math import gcd
 
 def get_random_coprime_to(x):
   while True:
-    y = number.getStrongPrime(x.bit_length())
+    y = getrandbits(x.bit_length())
     if y > 0 and gcd(x, y) == 1:
       return y
 
 def get_random_less_than(x):
-  return number.getRandomRange(1, x)
+  return getRandomRange(1, x)
